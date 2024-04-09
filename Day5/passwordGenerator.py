355c7288bee10ff336ff1b557f2cca477c220906
djshat8790 <dheeraj.shukla8790spain@gmail.com>
2024-04-09 16:49:51 +0200

    Day 5 closure

---
+++ Day5/passwordGenerator.py
@@ -0,0 +1,60 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password = ""
+for lot in range(0, nr_letters):
+    password += letters[random.randint(0, len(letters)-1)]
+
+for lot in range(0, nr_symbols):
+    password += symbols[random.randint(0, len(symbols)-1)]
+
+for lot in range(0, nr_numbers):
+    password += numbers[random.randint(0, len(numbers)-1)]
+
+print(f"{password}")
+
+password = ""
+for lot in range(0, nr_letters):
+    password += random.choice(letters)
+
+for lot in range(0, nr_symbols):
+    password += random.choice(symbols)
+
+for lot in range(0, nr_numbers):
+    password += random.choice(numbers)
+
+print(f"{password}")
+
+hard_password_list = []
+index = nr_numbers+nr_symbols+nr_letters
+
+for lot in range(0, nr_letters):
+    hard_password_list.insert(random.randint(0, index-1), letters[random.randint(0, len(letters)-1)])
+
+for lot in range(0, nr_symbols):
+    hard_password_list.insert(random.randint(0, index-1), symbols[random.randint(0, len(symbols)-1)])
+
+for lot in range(0, nr_numbers):
+    hard_password_list.insert(random.randint(0, index - 1), numbers[random.randint(0, len(numbers) - 1)])
+
+hard_password = ""
+for passwords in hard_password_list:
+    hard_password += passwords
+
+print(f"{hard_password}")
+
+# we could also use list.shuffle to reorders the list items in a list
